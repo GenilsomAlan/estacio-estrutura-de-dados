b8d9809ada1789a517955f07db42387ccb55ce44
G. Allan <90303422+GenilsomAlan@users.noreply.github.com>
2025-11-11 01:08:21 +0000

    modificação da variavel x fora da função main usando ponteiro

--- ponteiro.c
+++ ponteiro.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+
+void modificarX(int *p){
+    *p = *p + 2718;
+}
+
+
+int main(void){
+    int x = 3141;
+    int *p;
+    p = &x;
+    modificarX(p);
+
+    printf("%d",x);
+}
 
